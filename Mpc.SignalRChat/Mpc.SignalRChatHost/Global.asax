6bb8ccd6be6063ab384719b6103625c5b1c50c4e
Miguel Costa <miguelpintodacosta@gmail.com>
2016-04-12 15:35:55 +0100

    Add project to host chat

---
+++ Mpc.SignalRChat/Mpc.SignalRChatHost/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mpc.SignalRChatHost.MvcApplication" Language="C#" %>
---
+++ Mpc.SignalRChat/Mpc.SignalRChatHost/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Mpc.SignalRChatHost.Startup))]
+
+namespace Mpc.SignalRChatHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR("/mychat", new HubConfiguration());
+        }
+    }
+}
