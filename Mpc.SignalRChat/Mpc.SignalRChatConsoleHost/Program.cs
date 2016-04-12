d50e708ac4630e76870609e4d4ac8c1b684fddca
Miguel Costa <miguelpintodacosta@gmail.com>
2016-04-12 17:00:16 +0100

    Add self hosting project

---
+++ Mpc.SignalRChat/Mpc.SignalRChatConsoleHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mpc.SignalRChatConsoleHost
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using(WebApp.Start<Startup>("http://localhost:6789"))
+            {
+                Console.WriteLine("Hubs running...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
